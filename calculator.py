741ccdd284547ae39c68848ab166c5639850c2a6
ProtoSA <protopopovasvet@gmail.com>
2024-11-05 23:37:56 +0300

    Добавили операцию вычитания

--- calculator.py
+++ calculator.py
@@ -1,6 +1,9 @@
 def add(x, y):
     return x + y
 
+def sub(x, y):
+    return x - y
+
 print("Выберите операцию: 1-сложение, 2-вычитание, 3-умножение, 4-деление")
 operation = input("Введите номер операции:")
 n1 = int(input("Введите первое число:"))
@@ -8,3 +11,6 @@ n2 = int(input("Введите второе число:"))
 
 if operation == '1':
     print(f"{n1} + {n2} = {add(n1, n2)}")
+
+if operation == '2':
+    print(f"{n1} - {n2} = {sub(n1, n2)}")
